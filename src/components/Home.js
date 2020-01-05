359fb9322074a983993a5cf65dcf1136d5ef05c7
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-05 15:02:13 -0600

    adds search base & pop url, adds search functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
+import { POPULAR_BASE_URL,SEARCH_BASE_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 
 // import Components
 import HeroImage from './elements/HeroImage';
@@ -25,11 +25,17 @@ const Home = () => {
   if (error) return <div>Something went wrong ...</div>;
   if (!movies[0]) return <Spinner />;
     
+  const searchMovies = search => {
+    const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+    setSearchTerm(search);
+    fetchMovies(endpoint);
+  };
 
     const loadMoreMovies = () => {
       
-      const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`;
-      const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${searchTerm}&page=${currentPage + 1}`;
+      const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`;
+      const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
   
       const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
   
@@ -39,12 +45,14 @@ const Home = () => {
 
     return (
         <div>
+            {!searchTerm && (
             <HeroImage
             image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
             title={heroImage.original_title}
             text={heroImage.overview}
             />
-            <SearchBar/>
+            )}
+            <SearchBar callback={searchMovies}/>
             <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
           {movies.map(movie => (
             <MovieThumb
@@ -61,7 +69,7 @@ const Home = () => {
               </Grid>
             
             {loading && <Spinner/>}
-            {currentPage < totalPage && !loading && (
+            {currentPage < totalPages && !loading && (
             <LoadMoreBtn text="Load More" callback={loadMoreMovies}/>
             )}
         </div>
