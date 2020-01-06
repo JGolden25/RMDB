fe55d73b1e2957d501a68f6c011b8753f17ab4e6
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-05 18:21:17 -0600

    MOVIE refactored with React Hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,75 +10,30 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-class Movie extends Component {
-  state = { loading: true };
+import { useMovieFetch } from './hooks/useMovieFetch';
 
-  fetchData = async () => {
-    const { movieId } = this.props;
-    this.setState({ loading: true, error: false });
-
-    try {
-      const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
-      const result = await (await fetch(endpoint)).json();
-
-      const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
-      const creditsResult = await (await fetch(creditsEndpoint)).json();
-      const directors = creditsResult.crew.filter(
-        member => member.job === 'Director',
-      );
-
-      this.setState(
-        {
-          ...result,
-          actors: creditsResult.cast,
-          directors,
-          loading: false,
-        },
-        () => {
-          localStorage.setItem(movieId, JSON.stringify(this.state));
-        },
-      );
-    } catch (error) {
-      this.setState({ error: true });
-    }
-  };
-
-  componentDidMount() {
-    const { movieId } = this.props;
-    if (localStorage[movieId]) {
-      this.setState(JSON.parse(localStorage[movieId]));
-    } else {
-      this.fetchData();
-    }
-  }
-
-  render() {
-    const {
-      original_title: originalTitle,
-      runtime,
-      budget,
-      revenue,
-      actors,
-      error,
-      loading,
-    } = this.state;
-
+const Movie = ({ movieId }) => {
+    const [movie, loading, error] = useMovieFetch(movieId);
+  
     if (error) return <div>Something went wrong ...</div>;
     if (loading) return <Spinner />;
-
+  
     return (
-      <>
-        <Navigation movie={originalTitle} />
-        <MovieInfo movie={this.state} />
-        <MovieInfoBar time={runtime} budget={budget} revenue={revenue} />
-        <Grid header="Actors">
-          {actors.map(actor => (
-            <Actor key={actor.credit_id} actor={actor} />
-          ))}
-        </Grid>
-      </>
-    );
-  }
-}
-
-export default Movie;
+    <>
+      <Navigation movie={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header="Actors">
+        {movie.actors.map(actor => (
+          <Actor key={actor.credit_id} actor={actor} />
+        ))}     
+      </Grid>
+    </>
+    )
+  };
+  
+  export default Movie;
