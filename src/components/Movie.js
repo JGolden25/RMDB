eb1d75317686f79cba38862dc9c607f622c0ae3a
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-05 18:33:16 -0600

    adds styled Navbar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,7 +10,7 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-import { useMovieFetch } from './hooks/useMovieFetch';
+import { useMovieFetch } from './Hooks/useMovieFetch';
 
 const Movie = ({ movieId }) => {
     const [movie, loading, error] = useMovieFetch(movieId);
--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <div>
-            nav
-        </div>
-    );
-};
+import { Link } from '@reach/router';
+import PropTypes from 'prop-types';
+
+import { StyledNavigation } from '../styles/StyledNavigation';
+
+const Navigation = ({ movie }) => (
+    <StyledNavigation>
+      <div className="navigation-content">
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <p>|</p>
+        <p>{movie}</p>
+      </div>
+    </StyledNavigation>
+  );
+  
+  Navigation.propTypes = {
+    movie: PropTypes.string,
+  }
 
 export default Navigation;
